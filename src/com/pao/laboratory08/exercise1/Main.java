0995aadedabcb0621a561e6bf748985cfdd10093
Rattleface05 <andreidumitrescu7857@gmail.com>
2026-05-07 10:39:43 +0300

    Lab8: exercitiul 1 completat

--- src/com/pao/laboratory08/exercise1/Main.java
+++ src/com/pao/laboratory08/exercise1/Main.java
@@ -16,7 +16,54 @@ public class Main {
         //    - PRINT → afișează toți studenții
         //    - SHALLOW <nume> → shallow clone + modifică orașul clonei la "MODIFICAT" + afișează
         //    - DEEP <nume> → deep clone + modifică orașul clonei la "MODIFICAT" + afișează
+        Scanner in = new Scanner(System.in);
+        String[] action = in.nextLine().split(" ");
+        String[] sargs;
 
-        System.out.println("TODO: implementează exercițiul 1");
+        BufferedReader fin = new BufferedReader(new FileReader(FILE_PATH));
+
+        if (Objects.equals(action[0], "PRINT")) {
+            String linie;
+            while ((linie = fin.readLine()) != null) {
+                sargs = linie.split(",");
+
+                Student student = new Student(sargs[0],Integer.parseInt( sargs[1]),new Adresa(sargs[2],sargs[3]));
+                System.out.println(student);
+            }
+        } else if (Objects.equals(action[0], "SHALLOW")) {
+            String nume = action[1];
+            String linie;
+            while ((linie = fin.readLine()) != null) {
+                sargs = linie.split(",");
+                if (Objects.equals(sargs[0], nume)){
+                    Student student = new Student(sargs[0],Integer.parseInt( sargs[1]),new Adresa(sargs[2],sargs[3]));
+                    Student clone = (Student) student.clone();
+                    Adresa axu = clone.getAdresa();
+                    axu.setOras("MODIFICAT");
+                    clone.setAdresa(axu);
+                    System.out.println("Original: " + student);
+                    System.out.println("Clona: " + clone);
+
+                }
+            }
+        } else if (Objects.equals(action[0], "DEEP")) {
+            String nume = action[1];
+            String linie;
+            while ((linie = fin.readLine()) != null) {
+                sargs = linie.split(",");
+                if (Objects.equals(sargs[0], nume)){
+                    Student student = new Student(sargs[0],Integer.parseInt( sargs[1]),new Adresa(sargs[2],sargs[3]));
+                    Student clone = (Student) student.deppClone();
+                    Adresa axu = clone.getAdresa();
+                    axu.setOras("MODIFICAT");
+                    clone.setAdresa(axu);
+                    System.out.println("Original: " + student);
+                    System.out.println("Clona: " + clone);
+
+                }
+            }
+        }
+        fin.close();
+        //System.out.println("TODO: implementează exercițiul 1");
     }
 }
